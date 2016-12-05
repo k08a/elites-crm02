e9253488d4181ec7b48ba74bb6dbde409f5879ce
k08a <etnallirb.a@gmail.com>
2016-12-05 06:36:50 +0000

    indexアクション実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+100.times do |index|
+    Customer.create(family_name: "スズキ", given_name: "太郎", email: "customer_#{index}@sparta.com")
+    
+end
+    
+    
+    # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
